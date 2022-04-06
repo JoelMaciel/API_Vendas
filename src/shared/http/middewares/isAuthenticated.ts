d9920e1856a419f0d01900dbb295763936240676
Joel Maciel <soldado25461@gmail.com>
2022-04-06 13:17:49 -0300

    feat: :art: Multer
    Intalando e configurando arquivo do para fazer upload de arquivos utilizando a biblioteca Multer

--- src/shared/http/middewares/isAuthenticated.ts
+++ src/shared/http/middewares/isAuthenticated.ts
@@ -3,7 +3,7 @@ import { NextFunction, Request, Response } from 'express';
 import authConfig from '@config/auth';
 import { verify } from 'jsonwebtoken';
 
-interface TokenPayload {
+interface ITokenPayload {
   iat: number;
   exp: number;
   sub: string;
@@ -21,7 +21,7 @@ export default function isAuthenticated(
   const [, token] = authHeader.split(' ');
   try {
     const decodedToken = verify(token, authConfig.jwt.secret);
-    const { sub } = decodedToken as TokenPayload;
+    const { sub } = decodedToken as ITokenPayload;
 
     request.user = {
       id: sub

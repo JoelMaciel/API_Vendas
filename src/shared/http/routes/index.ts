13043046a51c44ba77930dcff0dde7375e882f86
Joel Maciel <soldado25461@gmail.com>
2022-04-05 22:08:46 -0300

    feat: :art: SessionController
    Criando rotas de autenticaçao de usuario

--- src/shared/http/routes/index.ts
+++ src/shared/http/routes/index.ts
@@ -1,9 +1,11 @@
 import productsRouter from '@modules/products/routes/Product.routes';
+import sessionRouter from '@modules/users/routes/session.routes';
 import userRouter from '@modules/users/routes/users.routes';
 import { Router } from 'express';
 
 const routes = Router();
 routes.use('/products', productsRouter);
 routes.use('/users', userRouter);
+routes.use('/sessions', sessionRouter);
 
 export default routes;
